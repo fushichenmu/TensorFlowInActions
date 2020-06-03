1c702f6266d4c83d3edbdf5f2b3c8767689a7245
chendongsheng <302663883@qq.com>
2020-06-03 11:14:28 +0800

    更新源码

--- chapter03/TF-Tensor.py
+++ chapter03/TF-Tensor.py
@@ -4,7 +4,20 @@ import tensorflow as tf
 # b =tf.constant([2.0,3.0],name='b')
 # result = tf.add(a,b,name='add')
 # print(result)
-a = tf.constant([1.0,2.0],name='a')
+
 b = tf.constant([2.0,3.0],name='b')
+a = tf.constant([1.0,2.0],name='cccc')
 result = a + b
+result2 = result*2
+
 print(tf.Session().run(result))
+print(tf.Session().run(result2))
+
+
+g1 = tf.Graph()
+with g1.as_default():
+    a = tf.constant([1.0, 2.0], name='ddddd')
+    b = tf.constant([2.0, 3.0], name='b')
+    result = a + b
+    print(result)
+print(a)
